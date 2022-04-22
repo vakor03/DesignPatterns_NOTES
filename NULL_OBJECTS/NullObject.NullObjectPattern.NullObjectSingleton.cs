ae82c568406ead8d1332a91dc389d1369a0a20c7
Vakor <vakor.dev@gmail.com>
2022-04-22 14:12:10 +0300

    Null objects

---
+++ NULL_OBJECTS/NullObject.NullObjectPattern.NullObjectSingleton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace NullObjectPattern
+{
+  interface ILog
+  {
+    public void Warn();
+
+    public static ILog Null => NullLog.Instance;
+
+    private sealed class NullLog : ILog
+    {
+      private NullLog() {}
+
+      private static Lazy<NullLog> instance =
+        new Lazy<NullLog>(() => new NullLog());
+
+      public static ILog Instance => instance.Value;
+
+      public void Warn()
+      {
+        
+      }
+    }
+  }
+
+  public class BankAccount
+  {
+    public BankAccount(ILog log = ILog.Null)
+    {
+      
+    }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ILog log = ILog.Null;
+    }
+  }
+}
